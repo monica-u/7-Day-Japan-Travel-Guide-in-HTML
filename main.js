68252808594a86f0f962088ada6271248728cd65
monica-u <monicau@hawaii.edu>
2022-06-05 19:53:17 -1000

    added buttons for each days' site

---
+++ main.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     console.log( "ready!" );
+//     $("button").html("Next Step...");
+//     $("button").on("click", functional(Event){
+//         .show()
+//     });
+// });
+$(document).ready(function() {
+        console.log( "ready!" );
+        //$("button").html("Next Step...");
+        $("button").click(function(){
+        location.href='https://c.tenor.com/LbVbdpTeMD0AAAAC/fooled-you-point.gif';
+
+    })})
